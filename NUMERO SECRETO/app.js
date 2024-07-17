121e55a1d18e8d6e64291fe1d63d463add897911
macaco vascaino <brenovitol501@gmail.com>
2024-07-17 17:11:35 -0300

    Update app.js

--- NUMERO SECRETO/app.js
+++ NUMERO SECRETO/app.js
@@ -3,7 +3,7 @@ let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
-function exibirTextNaTela(tag, texto) {
+function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
     responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
